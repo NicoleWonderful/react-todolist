df2837845e4f984d665342a0ef92aeeaeff24ffa
Nicole <qwepoi9633@gmail.com>
2019-06-28 00:21:45 +0800

    react redux todolist-demo

---
+++ precache-manifest.180e9622e43db1c7eac6534dc4aada69.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccfde99572e9352580c30575d17b9dab",
+    "url": "./index.html"
+  },
+  {
+    "revision": "dc0b1148c0319ff3b2ae",
+    "url": "./static/css/2.2a88c607.chunk.css"
+  },
+  {
+    "revision": "dc0b1148c0319ff3b2ae",
+    "url": "./static/js/2.8adb561b.chunk.js"
+  },
+  {
+    "revision": "fb3d26b369583703c5c8",
+    "url": "./static/js/main.9c12e532.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
